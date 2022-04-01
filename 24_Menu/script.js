1a42fa5675098b86567266164bf779811583eb0e
salihdemir2498 <salihhdemir1727@gmail.com>
2022-04-01 09:31:16 +0300

    Menu Projects

---
+++ 24_Menu/script.js
@@ -0,0 +1,135 @@
+
+let slideIndex = 0;
+let allSlides = $(".slide").toArray();
+let allDot=$(".dot").toArray();
+
+$(".nextSlide").click(function () {
+    // slideIndex++;
+    if (allSlides.length == slideIndex + 1) {
+        slideIndex = -1;
+    }
+    showSlide(++slideIndex); //1 arttırıp öyle çalışır
+})
+$(".prevSlide").click(function () {
+    // slideIndex--;
+    if (slideIndex == 0) {
+        slideIndex = allSlides.length;
+    }
+    showSlide(--slideIndex);
+})
+
+let showSlide = (n) => {
+    for (let i = 0; i < allSlides.length; i++) {
+        allSlides[i].style.display = "none";
+        allDot[i].classList.remove("active");
+    }
+    
+    allSlides[n].style.display = "block";
+    allDot[n].classList.add("active");
+
+
+    //ödev kayarak giden slider
+}
+
+$(".dot").mousemove(function () {
+    slideIndex = $(this).index();
+    showSlide(slideIndex);
+   
+
+})
+
+showSlide(slideIndex);
+
+//-------------
+
+
+
+
+$(document).ready(function(){
+    $(".img-1").click(function(){
+        $(".main-popup").css({
+            display: "block",
+               
+        })
+        $("#pop-1").css({
+            display: "block"    
+        })
+        $(".container").css({
+            opacity: 0.3   
+        })
+    })
+    $(".img-2").click(function(){
+        $(".main-popup").css({
+            display: "block",
+               
+        })
+        $("#pop-2").css({
+            display: "block"    
+        })
+        $(".container").css({
+            opacity: 0.3   
+        })
+    })
+    $(".img-3").click(function(){
+        $(".main-popup").css({
+            display: "block",
+               
+        })
+        $("#pop-3").css({
+            display: "block"    
+        })
+        $(".container").css({
+            opacity: 0.3   
+        })
+    })
+    $(".img-4").click(function(){
+        $(".main-popup").css({
+            display: "block",
+               
+        })
+        $("#pop-4").css({
+            display: "block"    
+        })
+        $(".container").css({
+            opacity: 0.3   
+        })
+    })
+    $(".img-5").click(function(){
+        $(".main-popup").css({
+            display: "block",
+               
+        })
+        $("#pop-5").css({
+            display: "block"    
+        })
+        $(".container").css({
+            opacity: 0.3   
+        })
+    })
+    $(".close-popup").click(function(){
+        $(".main-popup").css({
+            display: "none",
+               
+        })
+        $("#pop-5").css({
+            display: "none"    
+        })
+        $("#pop-4").css({
+            display: "none"    
+        })
+        $("#pop-3").css({
+            display: "none"    
+        })
+        $("#pop-2").css({
+            display: "none"    
+        })
+        $("#pop-1").css({
+            display: "none"    
+        })
+        $(".container").css({
+            opacity: 1   
+        })
+    })
+})
+
+
