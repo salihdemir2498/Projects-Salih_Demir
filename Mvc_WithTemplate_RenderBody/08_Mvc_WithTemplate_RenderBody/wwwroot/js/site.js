9c2bd67f78a18cd273e1f6514c5a162b1870c84d
salihdemir2498 <salihhdemir1727@gmail.com>
2022-04-06 10:04:04 +0300

    Mvc

---
+++ Mvc_WithTemplate_RenderBody/08_Mvc_WithTemplate_RenderBody/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿////alert("selam");
+
+$(document).ready(function () {
+    $("li a").each(function () {
+        if ($(this).attr("href") == window.location.pathname) {
+            $(this).addClass("activeLinkk");
+        }
+    })
+    //console.log($("li a").attr("href"));
+    //console.log(window.location.pathname);
+})
